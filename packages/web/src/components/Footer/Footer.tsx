2eca497b9acd7b3d4b541f8341b6d519915917fc
Yu Taek Lee <lcpnine@gmail.com>
2024-01-06 15:54:42 +0900

    :sparkles: Add footer

---
+++ packages/web/src/components/Footer/Footer.tsx
@@ -0,0 +1,22 @@
+const Footer = () => {
+  return (
+    <footer className="w-full bg-gray-200 py-4 mt-8">
+      <div className="text-center">
+        <p className="font-semibold m-2 text-gray-700">
+          We support your dreams
+        </p>
+        <p>
+          Contact:{' '}
+          <a
+            href="mailto:lcpnine@gmail.com"
+            className="text-blue-600 hover:text-blue-800"
+          >
+            lcpnine@gmail.com
+          </a>
+        </p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
