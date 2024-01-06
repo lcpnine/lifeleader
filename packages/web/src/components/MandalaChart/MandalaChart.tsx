ba0bc25856067a47bd3acca9b9beb1f1296719bd
Yu Taek Lee <lcpnine@gmail.com>
2024-01-06 23:14:51 +0900

    :fire: Remove outer border

--- packages/web/src/components/MandalaChart/MandalaChart.tsx
+++ packages/web/src/components/MandalaChart/MandalaChart.tsx
@@ -14,9 +14,7 @@ const MandalaChart = () => {
   }
 
   return (
-    <div
-      className={`grid grid-cols-3 gap-3 border ${themeStyle.gridBorder} max-w-fit`}
-    >
+    <div className={`grid grid-cols-3 gap-3 max-w-fit`}>
       {Array.from({ length: 9 }).map((_, index) => {
         const isCenterSquareGrid = index === 4
 
