6f0c41a1d8bf410336385e11cd3f7cb586c8f74b
Yu Taek Lee <lcpnine@gmail.com>
2024-01-06 15:42:36 +0900

    :sparkles: Distinguish layout component

--- packages/web/src/pages/_app.page.tsx
+++ packages/web/src/pages/_app.page.tsx
@@ -1,11 +1,14 @@
 import { ThemeProvider } from '@/contexts/theme/ThemeContext'
 import type { AppProps } from 'next/app'
 import './globals.css'
+import Layout from './layout'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <ThemeProvider>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ThemeProvider>
   )
 }
--- packages/web/src/pages/index.page.tsx
+++ packages/web/src/pages/index.page.tsx
@@ -3,11 +3,10 @@ import MandalaThemeSelector from '@/components/MandalaThemeSelector/MandalaTheme
 
 const Home = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100">
+    <div className="flex flex-col items-center justify-center min-h-screen p-4">
       <h1 className="text-5xl md:text-6xl lg:text-7xl font-extrabold text-center text-blue-600 my-4 shadow-lg">
         Mandala Chart
       </h1>
-
       <p className="text-center mb-6 max-w-prose">
         The <span className="font-bold">Mandala Chart</span> is a powerful tool
         for visualizing and achieving goals. It helps in organizing thoughts and
