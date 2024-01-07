72c9ae8960c616cfcafe93d40783c49a196a3144
Yu Taek Lee <lcpnine@gmail.com>
2024-01-08 00:21:27 +0900

    :sparkles: Commentize unused code

--- packages/web/src/components/MandalaChart/Square.i18n.ts
+++ packages/web/src/components/MandalaChart/Square.i18n.ts
@@ -7,10 +7,10 @@ const TRANSLATIONS = {
     mainGoal: '메인 목표',
     subGoal: '서브 목표',
   },
-  zh: {
-    mainGoal: '核心目标',
-    subGoal: '细项目标',
-  },
+  // zh: {
+  //   mainGoal: '核心目标',
+  //   subGoal: '细项目标',
+  // },
   'zh-Hant': {
     mainGoal: '核心目標',
     subGoal: '細項目標',
--- packages/web/src/components/ScreenshotButton/ScreenshotButton.i18n.ts
+++ packages/web/src/components/ScreenshotButton/ScreenshotButton.i18n.ts
@@ -5,9 +5,9 @@ const TRANSLATIONS = {
   ko: {
     download: '이미지 저장',
   },
-  zh: {
-    download: '下载图表',
-  },
+  // zh: {
+  //   download: '下载图表',
+  // },
   'zh-Hant': {
     download: '下載圖表',
   },
