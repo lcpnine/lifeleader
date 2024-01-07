d6deffde90fe8cb9155fcad8362e5c5c948f33fa
Yu Taek Lee <lcpnine@gmail.com>
2024-01-07 17:08:16 +0900

    :sparkles: Add google analytics

--- packages/web/src/pages/_app.page.tsx
+++ packages/web/src/pages/_app.page.tsx
@@ -1,15 +1,36 @@
 import { ThemeProvider } from '@/contexts/theme/ThemeContext'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 import './globals.css'
 import Layout from './layout'
 
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_MEASUREMENT_ID
+
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <ThemeProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <>
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+      />
+      <ThemeProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </>
   )
 }
 
