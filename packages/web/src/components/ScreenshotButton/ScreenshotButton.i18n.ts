ea103273cc4a0db9c1c52f3290760b25b64d3031
Yu Taek Lee <lcpnine@gmail.com>
2024-01-07 16:35:29 +0900

    :sparkles: Add i18n on screenshot button

---
+++ packages/web/src/components/ScreenshotButton/ScreenshotButton.i18n.ts
@@ -0,0 +1,10 @@
+const TRANSLATIONS = {
+  en: {
+    download: 'Download Chart',
+  },
+  ko: {
+    download: '이미지 저장',
+  },
+}
+
+export default TRANSLATIONS
--- packages/web/src/components/ScreenshotButton/ScreenshotButton.tsx
+++ packages/web/src/components/ScreenshotButton/ScreenshotButton.tsx
@@ -1,14 +1,27 @@
+import { SUPPORTING_LANGUAGES } from '@/constants/i18n'
+import useI18n from '@/hooks/useI18n'
+import { useRouter } from 'next/router'
+import TRANSLATIONS from './ScreenshotButton.i18n'
+
 interface Props {
   takeScreenShot: () => void
 }
 
 const ScreenshotButton = ({ takeScreenShot }: Props) => {
+  const { locale } = useRouter()
+  const { getTranslation } = useI18n()
+
+  const translation = getTranslation(
+    TRANSLATIONS,
+    locale as SUPPORTING_LANGUAGES
+  )
+
   return (
     <button
       className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition duration-300 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50"
       onClick={takeScreenShot}
     >
-      Download Chart
+      {translation('download')}
     </button>
   )
 }
