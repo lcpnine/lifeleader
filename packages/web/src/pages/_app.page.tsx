a2e84006e62234be057bbea39ce123168e5fe9ab
Yu Taek Lee <lcpnine@gmail.com>
2024-01-07 19:28:21 +0900

    :truck: Move metadata location

--- packages/web/src/pages/_app.page.tsx
+++ packages/web/src/pages/_app.page.tsx
@@ -1,6 +1,8 @@
 import { ThemeProvider } from '@/contexts/theme/ThemeContext'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import Script from 'next/script'
+import Favicon from 'public/favicon.ico'
 import './globals.css'
 import Layout from './layout'
 
@@ -9,6 +11,16 @@ const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_MEASUREMENT_ID
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
+      <Head>
+        <title>Life Leader</title>
+        <link rel="icon" href={Favicon.src} />
+        <meta charSet="utf-8" />
+        <meta name="description" content="Life Leader" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=0.6, maximum-scale=1"
+        />
+      </Head>
       <Script
         async
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
--- packages/web/src/pages/layout.tsx
+++ packages/web/src/pages/layout.tsx
@@ -1,16 +1,10 @@
 import Footer from '@/components/Footer/Footer'
 import Header from '@/components/Header/Header'
-import Head from 'next/head'
-import Favicon from 'public/favicon.ico'
 import { ReactNode } from 'react'
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <Head>
-        <title>Life Leader</title>
-        <link rel="icon" href={Favicon.src} />
-      </Head>
       <Header />
       <main>{children}</main>
       <Footer />
