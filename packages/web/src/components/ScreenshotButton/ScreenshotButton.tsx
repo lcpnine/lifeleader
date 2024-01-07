c0536a5e9af7852400f7b4092307b2261c8417c8
Yu Taek Lee <lcpnine@gmail.com>
2024-01-07 12:00:48 +0900

    :recycle: Change phrase

--- packages/web/src/components/ScreenshotButton/ScreenshotButton.tsx
+++ packages/web/src/components/ScreenshotButton/ScreenshotButton.tsx
@@ -8,7 +8,7 @@ const ScreenshotButton = ({ takeScreenShot }: Props) => {
       className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition duration-300 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50"
       onClick={takeScreenShot}
     >
-      Take Screenshot
+      Download Chart
     </button>
   )
 }
