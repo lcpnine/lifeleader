fcf5e6c012aecd5da66580318fda35b8322b6653
Yu Taek Lee <lcpnine@gmail.com>
2024-01-07 16:06:11 +0900

    :truck: Move file location

--- packages/web/src/components/Header/Header.tsx
+++ packages/web/src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import { LANGUAGE_NAMES_SHORT, SUPPORTING_LANGUAGES } from '@/constants/i18n'
 import useI18n from '@/hooks/useI18n'
 import { useRouter } from 'next/router'
-import LifeLeaderIcon from 'public/life-leader-icon.png'
+import LifeLeaderIcon from 'public/logo/image-only.png'
 
 const Header = () => {
   const { locale, locales } = useRouter()
