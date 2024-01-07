66c5c77d50ab190635b85185d539d815ec03eb05
Yu Taek Lee <lcpnine@gmail.com>
2024-01-07 16:26:25 +0900

    :sparkles: Change phrase on footer

--- packages/web/src/components/Footer/Footer.tsx
+++ packages/web/src/components/Footer/Footer.tsx
@@ -2,16 +2,14 @@ const Footer = () => {
   return (
     <footer className="w-full bg-gray-200 py-4 mt-8">
       <div className="text-center">
-        <p className="font-semibold m-2 text-gray-700">
-          We support your dreams
-        </p>
+        <p className="font-semibold m-2 text-gray-700">Achieve Your Dreams</p>
         <p>
           Contact:{' '}
           <a
-            href="mailto:lcpnine@gmail.com"
+            href="mailto:life.leader.me@gmail.com"
             className="text-blue-600 hover:text-blue-800"
           >
-            lcpnine@gmail.com
+            life.leader.me@gmail.com
           </a>
         </p>
       </div>
