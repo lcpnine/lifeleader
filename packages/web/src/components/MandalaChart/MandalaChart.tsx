634921f6b14ecd0061db6581260181a150f92ebb
Yu Taek Lee <lcpnine@gmail.com>
2024-01-11 00:54:48 +0900

    :sparkles: Show arrow

--- packages/web/src/components/MandalaChart/MandalaChart.tsx
+++ packages/web/src/components/MandalaChart/MandalaChart.tsx
@@ -1,6 +1,16 @@
 import { DIMMED_Z_INDEX } from '@/constants/common'
 import { MandalaChartView } from '@/constants/mandalaChart'
 import { useState } from 'react'
+import {
+  ImArrowDown,
+  ImArrowDownLeft,
+  ImArrowDownRight,
+  ImArrowLeft,
+  ImArrowRight,
+  ImArrowUp,
+  ImArrowUpLeft,
+  ImArrowUpRight,
+} from 'react-icons/im'
 import { deepCopy } from '../../../utils/common'
 import Grid from './Grid'
 
@@ -54,16 +64,59 @@ const MandalaChart = ({ viewOption, screenShotRef }: Props) => {
           Back
         </button>
       )}
-      <div className={`grid grid-cols-3 gap-3`}>
-        {wholeGridValues.map((_, index) => (
-          <Grid
-            key={index}
-            wholeGridValues={wholeGridValues}
-            handleGridValue={handleGridValue}
-            gridIndex={index}
-          />
-        ))}
-      </div>
+      {viewOption === MandalaChartView.FULL_VIEW ? (
+        <div className={`grid grid-cols-3 gap-3`}>
+          {wholeGridValues.map((_, index) => (
+            <Grid
+              key={index}
+              wholeGridValues={wholeGridValues}
+              handleGridValue={handleGridValue}
+              gridIndex={index}
+            />
+          ))}
+        </div>
+      ) : (
+        <div className="flex-col justify-center align-bottom">
+          <div className="flex w-full justify-between">
+            <div className="flex justify-center items-center basis-[3px]">
+              <ImArrowUpLeft size="2em" />
+            </div>
+            <div className="flex justify-center items-center basis-[3px]">
+              <ImArrowUp size="2em" />
+            </div>
+            <div className="flex justify-center items-center basis-[3px]">
+              <ImArrowUpRight size="2em" />
+            </div>
+          </div>
+          <div className="flex w-fit">
+            <div className="flex justify-center items-center basis-[3px]">
+              <ImArrowLeft size="2em" />
+            </div>
+            <div className="flex justify-center items-center">
+              <Grid
+                key={'init'}
+                wholeGridValues={wholeGridValues}
+                handleGridValue={handleGridValue}
+                gridIndex={4}
+              />
+            </div>
+            <div className="flex justify-center items-center basis-[3px]">
+              <ImArrowRight size="2em" />
+            </div>
+          </div>
+          <div className="flex w-full justify-between">
+            <div className="flex justify-center items-center basis-[3px]">
+              <ImArrowDownLeft size="2em" />
+            </div>
+            <div className="flex justify-center items-center basis-[3px]">
+              <ImArrowDown size="2em" />
+            </div>
+            <div className="flex justify-center items-center basis-[3px]">
+              <ImArrowDownRight size="2em" />
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
