09003bf0831ccd4ee49b95f2813e20fd1835048a
Yu Taek Lee <lcpnine@gmail.com>
2024-01-11 01:01:36 +0900

    :fire: Remove unnecessary code

--- packages/web/src/components/MandalaChart/MandalaChart.tsx
+++ packages/web/src/components/MandalaChart/MandalaChart.tsx
@@ -78,18 +78,18 @@ const MandalaChart = ({ viewOption, screenShotRef }: Props) => {
       ) : (
         <div className="flex-col justify-center align-bottom">
           <div className="flex w-full justify-between">
-            <div className="flex justify-center items-center basis-[3px]">
+            <div className="flex justify-center items-center">
               <BiSolidLeftTopArrowCircle size="2em" />
             </div>
-            <div className="flex justify-center items-center basis-[3px]">
+            <div className="flex justify-center items-center">
               <BiSolidUpArrowCircle size="2em" />
             </div>
-            <div className="flex justify-center items-center basis-[3px]">
+            <div className="flex justify-center items-center">
               <BiSolidRightTopArrowCircle size="2em" />
             </div>
           </div>
           <div className="flex w-fit">
-            <div className="flex justify-center items-center basis-[3px]">
+            <div className="flex justify-center items-center">
               <BiSolidLeftArrowCircle size="2em" />
             </div>
             <div className="flex justify-center items-center m-4">
@@ -100,18 +100,18 @@ const MandalaChart = ({ viewOption, screenShotRef }: Props) => {
                 gridIndex={4}
               />
             </div>
-            <div className="flex justify-center items-center basis-[3px]">
+            <div className="flex justify-center items-center">
               <BiSolidRightArrowCircle size="2em" />
             </div>
           </div>
           <div className="flex w-full justify-between">
-            <div className="flex justify-center items-center basis-[3px]">
+            <div className="flex justify-center items-center">
               <BiSolidLeftDownArrowCircle size="2em" />
             </div>
-            <div className="flex justify-center items-center basis-[3px]">
+            <div className="flex justify-center items-center">
               <BiSolidDownArrowCircle size="2em" />
             </div>
-            <div className="flex justify-center items-center basis-[3px]">
+            <div className="flex justify-center items-center">
               <BiSolidRightDownArrowCircle size="2em" />
             </div>
           </div>
