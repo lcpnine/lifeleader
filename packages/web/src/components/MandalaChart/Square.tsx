619ae7f9f8b8d0a80c13945c1ed47dec20dd8065
Yu Taek Lee <lcpnine@gmail.com>
2024-01-12 17:50:13 +0900

    :bug: Set background color for text area

--- packages/web/src/components/MandalaChart/Square.tsx
+++ packages/web/src/components/MandalaChart/Square.tsx
@@ -79,8 +79,8 @@ const Square = ({
       className={`${isMobile ? 'size-20' : 'size-24'} border ${
         themeStyle.borderColor
       } flex items-center justify-center overflow-hidden ${
-        themeStyle.backgroundColor
-      } ${isGridValid ? '' : 'bg-opacity-25 pointer-events-none'} ${
+        isGridValid ? '' : 'bg-opacity-25 pointer-events-none'
+      } ${
         isGridValid && !isCenterGrid && isCenterSquare
           ? 'pointer-events-none'
           : ''
@@ -92,7 +92,9 @@ const Square = ({
         onBlur={onBlurTextArea}
         onDoubleClick={handleDoubleClick}
         disabled={!isGridValid || (!isCenterGrid && isCenterSquare)}
-        className={`w-full h-full text-center ${textColor} ${textBold} p-0 cursor-text resize-none overflow-auto focus:outline-none`}
+        className={`w-full h-full text-center ${textColor} ${textBold} 
+          ${themeStyle.backgroundColor} p-0 cursor-text resize-none
+          overflow-auto focus:outline-none`}
         placeholder={placeHolder}
         style={{ whiteSpace: 'pre-wrap' }}
       />
