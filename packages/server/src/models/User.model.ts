529b33a029bc02229745a86340585fad7e9570b3
Yu Taek Lee <lcpnine@gmail.com>
2024-01-13 18:04:35 +0900

    :fire: Remove double hashing

--- packages/server/src/models/User.model.ts
+++ packages/server/src/models/User.model.ts
@@ -1,4 +1,3 @@
-import bcrypt from 'bcryptjs'
 import mongoose, { Document, Schema } from 'mongoose'
 
 export interface IUser extends Document {
@@ -11,13 +10,6 @@ const UserSchema: Schema = new Schema({
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   nickname: String,
-})
-
-UserSchema.pre<IUser>('save', async function (next) {
-  if (!this.isModified('password')) return next()
-
-  this.password = await bcrypt.hash(this.password, 10)
-  next()
 })
 
 export default mongoose.model<IUser>('User', UserSchema)
