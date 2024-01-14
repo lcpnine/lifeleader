77a138d5be7fd8bb56cb0586dac6b0419d3b4b5b
Yu Taek Lee <lcpnine@gmail.com>
2024-01-15 03:08:41 +0900

    :sparkles: Apply i18n on alert

---
+++ packages/web/src/components/Modal/Alert.i18n.ts
@@ -0,0 +1,13 @@
+const TRANSLATIONS = {
+  en: {
+    confirm: 'Confirm',
+  },
+  ko: {
+    confirm: '확인',
+  },
+  'zh-Hant': {
+    confirm: '確認',
+  },
+}
+
+export default TRANSLATIONS
--- packages/web/src/components/Modal/Alert.tsx
+++ packages/web/src/components/Modal/Alert.tsx
@@ -1,9 +1,15 @@
+import useI18n from '@/hooks/useI18n'
+import TRANSLATIONS from './Alert.i18n'
+
 interface Props {
   text: string
   closeModal: () => void
 }
 
 const Alert = ({ text, closeModal }: Props) => {
+  const { getTranslation } = useI18n()
+  const translation = getTranslation(TRANSLATIONS)
+
   return (
     <div className="bg-white p-4 rounded flex flex-col items-center">
       <p className="mb-4 text-center">{text}</p>
@@ -11,7 +17,7 @@ const Alert = ({ text, closeModal }: Props) => {
         onClick={closeModal}
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
       >
-        Confirm
+        {translation('confirm')}
       </button>
     </div>
   )
