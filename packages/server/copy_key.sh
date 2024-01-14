70e3466ab855980cc64e295f452fb603dcd41f49
Yu Taek Lee <lcpnine@gmail.com>
2024-01-15 02:29:50 +0900

    :fire: Remove unnecessary file

--- packages/server/copy_key.sh
+++
@@ -1,2 +0,0 @@
-sudo cp /etc/letsencrypt/live/api.lifeleader.me/privkey.pem /home/opc/lifeleader/packages/server/keys/privkey.pem
-sudo cp /etc/letsencrypt/live/api.lifeleader.me/fullchain.pem /home/opc/lifeleader/packages/server/keys/fullchain.pem
