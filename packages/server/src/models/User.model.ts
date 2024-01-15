0dbc714a2f282166c72b5dee417d049caa1e0463
Yu Taek Lee <lcpnine@gmail.com>
2024-01-15 20:41:36 +0900

    :sparkles: Add reset password field on user

--- packages/server/src/models/User.model.ts
+++ packages/server/src/models/User.model.ts
@@ -1,15 +1,25 @@
 import mongoose, { Document, Schema } from 'mongoose'
 
+export interface ResetPassword {
+  token: string | null
+  expires: Date | null
+}
+
 export interface IUser extends Document {
   email: string
   password: string
   nickname: string
+  resetPassword: ResetPassword
 }
 
 const UserSchema: Schema = new Schema({
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   nickname: { type: String, required: true },
+  resetPassword: {
+    token: { type: String, default: null },
+    expires: { type: Date, default: null },
+  },
 })
 
 export default mongoose.model<IUser>('User', UserSchema)
