74e6ce057f93e43f55e4957a276d86783542f572
Yu Taek Lee <lcpnine@gmail.com>
2024-01-16 00:00:48 +0900

    :lipstick: Change style

--- packages/web/src/components/Footer/Footer.tsx
+++ packages/web/src/components/Footer/Footer.tsx
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
-    <footer className="w-full bg-gray-200 py-4 mt-8">
+    <footer className="w-full bg-gray-200 py-4">
       <div className="text-center">
-        <p className="font-semibold m-2 text-gray-700">Achieve Your Dreams</p>
-        <p>
+        <p className="font-semibold mb-2 text-gray-700">Achieve Your Dreams</p>
+        <p className="mb-2">
           Contact:{' '}
           <a
             href="mailto:life.leader.me@gmail.com"
