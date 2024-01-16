c4d53fbc211b8b536c4d9a2c8c1946551ea500da
Yu Taek Lee <lcpnine@gmail.com>
2024-01-17 01:30:33 +0900

    :sparkles: Add created At field

--- packages/server/src/models/User.model.ts
+++ packages/server/src/models/User.model.ts
@@ -16,6 +16,7 @@ const UserSchema: Schema = new Schema({
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   nickname: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
   resetPassword: {
     token: { type: String, default: null },
     expires: { type: Date, default: null },
