5b5e1438026ce89596711f3536e3f409da041a80
Yu Taek Lee <lcpnine@gmail.com>
2024-01-17 01:38:01 +0900

    :sparkles: Add created At field

--- packages/server/src/models/User.model.ts
+++ packages/server/src/models/User.model.ts
@@ -5,17 +5,25 @@ export interface ResetPassword {
   expires: Date | null
 }
 
+export interface PurchasedInfo {
+  paid: boolean
+  boughtAt: Date | null
+  expiresAt: Date | null
+}
+
 export interface IUser extends Document {
   email: string
   password: string
   nickname: string
   resetPassword: ResetPassword
+  purchasedInfo: PurchasedInfo
 }
 
 const UserSchema: Schema = new Schema({
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   nickname: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
   resetPassword: {
     token: { type: String, default: null },
     expires: { type: Date, default: null },
