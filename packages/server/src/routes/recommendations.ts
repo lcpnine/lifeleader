db32f614689acc4a88492279dad943f15f315d8a
Yu Taek Lee <lcpnine@gmail.com>
2024-01-16 21:27:38 +0900

    :sparkles: Add recommendations router

---
+++ packages/server/src/routes/recommendations.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from 'express'
+
+const router = express.Router()
+
+router.post('/sub-goals', async (req: Request, res: Response) => {
+  const { mainGoal, subGoals } = req.body
+  const recommendations = [
+    'You can do it!',
+    "You're doing great!",
+    'Keep it up!',
+    "You're almost there!",
+    "You're almost done!",
+  ]
+
+  res
+    .status(200)
+    .json({ message: 'User successfully registered', user: recommendations })
+})
