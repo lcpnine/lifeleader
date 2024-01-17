b3b9b79b00aea85b077079468b1f0cfe09af8ace
Yu Taek Lee <lcpnine@gmail.com>
2024-01-18 02:00:40 +0900

    :sparkles: Add condition

--- packages/server/src/middlewares/preventUnpaidUser.ts
+++ packages/server/src/middlewares/preventUnpaidUser.ts
@@ -1,8 +1,8 @@
 import { NextFunction, Request, Response } from 'express'
 import { IUser } from '../models/User.model'
 const preventUnpaidUser = (req: Request, res: Response, next: NextFunction) => {
-  const user = req.user as IUser
-  if (!user.purchasedInfo.isPurchased) {
+  const user = req.user as IUser | undefined
+  if (!user || !user.purchasedInfo.isPurchased) {
     res.status(403).json({
       message: 'You are not allowed to access this resource',
     })
