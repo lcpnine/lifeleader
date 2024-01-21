d22fa6c3cc0a5c63281a0f34982ded271d9eea7b
Yu Taek Lee <lcpnine@gmail.com>
2024-01-22 01:38:02 +0900

    :sparkles: Set loopback on trust proxy

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -28,7 +28,7 @@ console.log('PHASE: ', PHASE)
 initializePassport(passport)
 
 const app = express()
-app.set('trust proxy', 1)
+app.set('trust proxy', 'loopback')
 
 const redisClient = createClient({
   socket: {
