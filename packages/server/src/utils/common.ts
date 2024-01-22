dc969ab7e3b22b2190af18df6cd3749a545544e2
Yu Taek Lee <lcpnine@gmail.com>
2024-01-23 02:07:19 +0900

    :sparkles: Make checking password valid function return true only on dev phase

--- packages/server/src/utils/common.ts
+++ packages/server/src/utils/common.ts
@@ -1,4 +1,7 @@
+import { IS_DEV } from '../constant/common'
+
 export const isPasswordValid = (password: string) => {
+  if (IS_DEV) return true
   const regex =
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
   return regex.test(password)
