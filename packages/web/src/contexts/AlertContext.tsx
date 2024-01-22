bbe7c774a142f819f68b1fbf941803d41514c7c0
Yu Taek Lee <lcpnine@gmail.com>
2024-01-23 01:22:12 +0900

    :sparkles: Set onClose on use alert

--- packages/web/src/contexts/AlertContext.tsx
+++ packages/web/src/contexts/AlertContext.tsx
@@ -2,8 +2,13 @@ import Alert from '@/components/Modal/Alert'
 import useModal from '@/hooks/useModal'
 import { ReactNode, createContext, useContext, useState } from 'react'
 
+interface OpenAlertProps {
+  text: string
+  onClose?: (e?: Event) => void
+}
+
 const DefaultAlertContext = {
-  openAlert: (text: string) => {},
+  openAlert: ({ text, onClose = () => {} }: OpenAlertProps) => {},
 }
 
 const AlertContext = createContext(DefaultAlertContext)
@@ -12,6 +17,7 @@ export const useAlert = () => useContext(AlertContext)
 
 export const AlertProvider = ({ children }: { children: ReactNode }) => {
   const [alertProps, setAlertProps] = useState({ text: '' })
+  const [onClose, setOnClose] = useState<() => void>((e?: Event) => {})
 
   const { ModalComponent, openModal } = useModal({
     Modal: Alert,
@@ -19,11 +25,13 @@ export const AlertProvider = ({ children }: { children: ReactNode }) => {
       text: alertProps.text,
     },
     onModalClose: () => {
+      onClose()
       setAlertProps({ text: '' })
     },
   })
 
-  const openAlert = (text: string) => {
+  const openAlert = ({ text, onClose = () => {} }: OpenAlertProps) => {
+    setOnClose(onClose)
     openModal({ text })
   }
 
