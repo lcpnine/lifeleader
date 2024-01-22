1d44f16ad50fea44aa5eeeb1709c518a404be735
Yu Taek Lee <lcpnine@gmail.com>
2024-01-23 01:59:03 +0900

    :sparkles: Add TokenInfo interface and emailVerification field on IUser

--- packages/server/src/models/User.model.ts
+++ packages/server/src/models/User.model.ts
@@ -1,38 +1,32 @@
 import mongoose, { Document, Schema } from 'mongoose'
 
-export interface ResetPassword {
+export interface TokenInfo {
   token: string | null
   expires: Date | null
-}
-
-export interface PurchasedInfo {
-  isPurchased: boolean
-  purchasedAt: Date | null
-  expiresAt: Date | null
+  isVerified?: boolean
 }
 
 export interface IUser extends Document {
   email: string
   password: string
   nickname: string
-  resetPassword: ResetPassword
-  purchasedInfo: PurchasedInfo
+  emailVerification: TokenInfo
+  resetPassword: TokenInfo
 }
+
+const tokenInfoSchema: Schema = new Schema({
+  token: { type: String, default: null },
+  expires: { type: Date, default: null },
+  isVerified: { type: Boolean, default: false },
+})
 
 const UserSchema: Schema = new Schema({
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   nickname: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
-  resetPassword: {
-    token: { type: String, default: null },
-    expires: { type: Date, default: null },
-  },
-  purchasedInfo: {
-    isPurchased: { type: Boolean, default: false },
-    purchasedAt: { type: Date, default: null },
-    expiresAt: { type: Date, default: null },
-  },
+  emailVerification: { type: tokenInfoSchema, default: () => ({}) },
+  resetPassword: { type: tokenInfoSchema, default: () => ({}) },
 })
 
 export default mongoose.model<IUser>('User', UserSchema)
