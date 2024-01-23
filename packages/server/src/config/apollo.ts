7e2c90d00967bc22fcbbf4876736ad08f86212b4
Yu Taek Lee <lcpnine@gmail.com>
2024-01-23 14:40:32 +0900

    :sparkles: Build schema

--- packages/server/src/config/apollo.ts
+++ packages/server/src/config/apollo.ts
@@ -1,9 +1,21 @@
 import { ApolloServerExpressConfig } from 'apollo-server-express'
+import { buildSchema } from 'type-graphql'
+import { UserResolver } from '../resolvers/user.resolver'
 
-const apolloConfig: ApolloServerExpressConfig = {
-  typeDefs: undefined,
-  resolvers: undefined,
-  context: ({ req, res }) => ({ req, res }), // Passes Express request object to resolvers
+const createSchema = async () =>
+  await buildSchema({
+    resolvers: [UserResolver],
+  })
+
+const createApolloConfig = async () => {
+  const schema = await createSchema()
+
+  const apolloConfig: ApolloServerExpressConfig = {
+    schema,
+    context: ({ req, res }) => ({ req, res }),
+  }
+
+  return apolloConfig
 }
 
-export default apolloConfig
+export default createApolloConfig
