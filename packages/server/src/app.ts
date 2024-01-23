f535674601318aa9b6b3d09b65ea2ad4d1011a2b
Yu Taek Lee <lcpnine@gmail.com>
2024-01-23 14:42:51 +0900

    :sparkles: Run apollo server on app and run remove unnecessary files and dependencies

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -8,111 +8,67 @@ configDotenv({
 
 import { ApolloServer } from 'apollo-server-express'
 import bodyParser from 'body-parser'
-import RedisStore from 'connect-redis'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
-import express, { NextFunction, Request, Response } from 'express'
-import expressSession from 'express-session'
+import express from 'express'
 import mongoose from 'mongoose'
-import passport from 'passport'
-import { createClient } from 'redis'
-import apolloConfig from './config/apollo'
-import initializePassport from './config/passport'
-import { COOKIE_DOMAIN, IS_DEV, PHASE } from './constant/common'
-import healthCheckController from './controllers/healthCheck'
-import { IUser } from './models/User.model'
-import authRoutes from './routes/auth'
-import recommendationRoutes from './routes/recommendation'
-import testRoutes from './routes/test'
+import createApolloConfig from './config/apollo'
+import { IS_DEV, PHASE } from './constant/common'
 
-console.log('PHASE: ', PHASE)
+const startApp = async () => {
+  console.log('PHASE: ', PHASE)
 
-initializePassport(passport)
+  const app = express()
+  app.set('trust proxy', 'loopback')
 
-const app = express()
-app.set('trust proxy', 'loopback')
+  app.use(express.json())
+  app.use(cookieParser())
 
-const redisClient = createClient({
-  socket: {
-    host: 'localhost',
-    port: 6379,
-  },
-  password: process.env.REDIS_PASSWORD,
-})
-redisClient.connect()
-redisClient.on('error', function (err) {
-  console.log('Could not establish a connection with Redis. ' + err)
-})
+  app.use(bodyParser.urlencoded({ extended: false }))
+  app.use(bodyParser.json())
 
-redisClient.on('connect', function () {
-  console.log('Connected to Redis successfully')
-})
+  const corsOrigin = IS_DEV
+    ? 'http://localhost:3000'
+    : 'https://www.lifeleader.me'
+  app.use(
+    cors({
+      origin: corsOrigin,
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      credentials: true,
+    })
+  )
 
-const redisStore = new RedisStore({
-  client: redisClient,
-})
+  mongoose.connect(process.env.MONGO_URI as string)
 
-app.use(express.json())
-app.use(
-  expressSession({
-    store: redisStore,
-    secret: process.env.SESSION_SECRET as string,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      httpOnly: true,
-      maxAge: 2 * 60 * 60 * 1000,
-      secure: !IS_DEV,
-      domain: COOKIE_DOMAIN,
-    },
+  const apolloConfig = await createApolloConfig()
+  const apolloServer = new ApolloServer(apolloConfig)
+  await apolloServer.start()
+  apolloServer.applyMiddleware({ app })
+
+  // app.use('/auth', authRoutes)
+  // app.use(
+  //   '/recommendation',
+  //   passport.authenticate(
+  //     'session',
+  //     (req: Request, res: Response, next: NextFunction) => {
+  //       const user = req.user as IUser | undefined
+  //       if (user?.purchasedInfo.isPurchased) {
+  //         next()
+  //       } else {
+  //         res.status(403).json({ message: 'Not purchased' })
+  //       }
+  //     }
+  //   ),
+  //   recommendationRoutes
+  // )
+  // app.use('/test', passport.authenticate('session'), testRoutes)
+  // app.get('/health-check', healthCheckController.get)
+
+  const PORT = process.env.PORT || 4003
+
+  app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`)
   })
-)
-app.use(cookieParser())
+}
 
-app.use(passport.initialize())
-app.use(passport.session())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
-const corsOrigin = IS_DEV
-  ? 'http://localhost:3000'
-  : 'https://www.lifeleader.me'
-app.use(
-  cors({
-    origin: corsOrigin,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    credentials: true,
-  })
-)
-
-mongoose.connect(process.env.MONGO_URI as string)
-
-const apolloServer = new ApolloServer(apolloConfig)
-apolloServer.applyMiddleware({ app })
-
-app.use('/auth', authRoutes)
-app.use(
-  '/recommendation',
-  passport.authenticate(
-    'session',
-    (req: Request, res: Response, next: NextFunction) => {
-      const user = req.user as IUser | undefined
-      if (user?.purchasedInfo.isPurchased) {
-        next()
-      } else {
-        res.status(403).json({ message: 'Not purchased' })
-      }
-    }
-  ),
-  recommendationRoutes
-)
-app.use('/test', passport.authenticate('session'), testRoutes)
-app.get('/health-check', healthCheckController.get)
-
-const PORT = process.env.PORT || 4003
-
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`)
-})
-
-export default app
+startApp()
