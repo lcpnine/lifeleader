117d41affe2d32ff6a316bbd1c921a4de14d60de
Yu Taek Lee <lcpnine@gmail.com>
2024-01-23 16:58:27 +0900

    :sparkles: Add keep signed in arg on sign in mutation

--- packages/server/src/resolvers/user.resolver.ts
+++ packages/server/src/resolvers/user.resolver.ts
@@ -2,10 +2,7 @@ import bcrypt from 'bcryptjs'
 import crypto from 'crypto'
 import jwt from 'jsonwebtoken'
 import { Arg, Mutation, Query, Resolver } from 'type-graphql'
-import {
-  createEmailVerificationTemplate,
-  createResetPasswordTemplate,
-} from '../constant/nodemailer'
+import { createEmailVerificationTemplate } from '../constant/nodemailer'
 import UserModel, { IUser } from '../models/User.model'
 import { User } from '../types/user'
 import { isPasswordValid } from '../utils/common'
@@ -26,7 +23,8 @@ export class UserResolver {
   @Mutation(() => SignInSuccessResponse || SignInFailResponse)
   async signIn(
     @Arg('email') email: string,
-    @Arg('password') password: string
+    @Arg('password') password: string,
+    @Arg('keepSignedIn') keepSignedIn: boolean = false
   ): Promise<SignInSuccessResponse | SignInFailResponse> {
     const user = await UserModel.findOne({ email })
     if (!user) throw new Error('User not found')
@@ -38,7 +36,7 @@ export class UserResolver {
       { userId: user._id },
       process.env.JWT_SECRET as string,
       {
-        expiresIn: '1h',
+        expiresIn: keepSignedIn ? '10y' : '1h',
       }
     )
 
