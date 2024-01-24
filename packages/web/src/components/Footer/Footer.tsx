534bde4a5484cfbd96fc53558e9ac81efa0c71a3
Yu Taek Lee <lcpnine@gmail.com>
2024-01-24 22:48:45 +0900

    :sparkles: Add sign out and delete account button on footer

--- packages/web/src/components/Footer/Footer.tsx
+++ packages/web/src/components/Footer/Footer.tsx
@@ -1,17 +1,48 @@
+import { useUserContext } from '@/contexts/UserContext'
+import useSignOut from '@/hooks/useSignOut/useSignOut'
+import { useRouter } from 'next/router'
+
 const Footer = () => {
+  const router = useRouter()
+  const signOut = useSignOut()
+  const { isSignedIn } = useUserContext()
+
+  const handleLogout = () => {
+    signOut().then(() => {
+      router.push('/')
+    })
+  }
+
   return (
     <footer className="w-full bg-gray-200 py-4">
       <div className="text-center">
         <p className="font-semibold mb-2 text-gray-700">Achieve Your Dreams</p>
-        <p className="mb-2">
-          Contact:{' '}
+        <div className="inline-flex gap-4 justify-center">
+          {isSignedIn && (
+            <>
+              <button
+                onClick={handleLogout}
+                className="text-gray-600 hover:text-gray-800 hover:underline"
+              >
+                Log Out
+              </button>
+              <span className="text-gray-600">|</span>
+              <a
+                href="/auth/delete-account"
+                className="text-gray-600 hover:text-gray-800 hover:underline"
+              >
+                Delete Account
+              </a>
+              <span className="text-gray-600">|</span>
+            </>
+          )}
           <a
             href="mailto:life.leader.me@gmail.com"
-            className="text-blue-600 hover:text-blue-800"
+            className="text-gray-600 hover:text-gray-800 hover:underline"
           >
-            life.leader.me@gmail.com
+            Contact Us
           </a>
-        </p>
+        </div>
       </div>
     </footer>
   )
