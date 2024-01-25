298c910055972812606c189e0d24c21eb4fd1c3a
Yu Taek Lee <lcpnine@gmail.com>
2024-01-25 22:20:44 +0900

    :sparkles: Add resolver for mandala chart

--- packages/server/src/resolvers/mandalaChart.resolver.ts
+++ packages/server/src/resolvers/mandalaChart.resolver.ts
@@ -1,13 +1,68 @@
-import { Arg, Query, Resolver } from 'type-graphql'
-import { IMandalaChart, MandalaChartModel } from '../models/MandalaChart.model'
-import { MandalaChart } from '../types/mandalaChart'
+// server/src/resolvers/mandalaChart.resolver.ts
+import { Arg, Mutation, Query, Resolver } from 'type-graphql'
+import { MandalaChartModel } from '../models/MandalaChart.model'
+import {
+  CreateMandalaChartInput,
+  CreateMandalaChartResponse,
+  DeleteMandalaChartFailureType,
+  DeleteMandalaChartInput,
+  DeleteMandalaChartResponse,
+  GetMandalaChartFailureType,
+  GetMandalaChartInput,
+  GetMandalaChartResponse,
+  UpdateMandalaChartFailureType,
+  UpdateMandalaChartInput,
+  UpdateMandalaChartResponse,
+} from './dto/mandalaChart.dto'
 
 @Resolver()
 export class MandalaChartResolver {
-  @Query(() => MandalaChart, { nullable: true })
+  @Query(() => GetMandalaChartResponse)
   async getMandalaChart(
-    @Arg('_id') _id: string
-  ): Promise<IMandalaChart | null> {
-    return await MandalaChartModel.findById(_id)
+    @Arg('input') input: GetMandalaChartInput
+  ): Promise<typeof GetMandalaChartResponse> {
+    const mandalaChart = await MandalaChartModel.findById(input.mandalaChartId)
+      .populate('centerCell')
+      .populate('surroundingCells')
+    if (!mandalaChart) {
+      return { errorType: GetMandalaChartFailureType.CHART_NOT_FOUND }
+    }
+    return { mandalaChart }
+  }
+
+  @Mutation(() => CreateMandalaChartResponse)
+  async createMandalaChart(
+    @Arg('input') input: CreateMandalaChartInput
+  ): Promise<typeof CreateMandalaChartResponse> {
+    const mandalaChart = await MandalaChartModel.create(input)
+    return { _id: mandalaChart._id }
+  }
+
+  @Mutation(() => UpdateMandalaChartResponse)
+  async updateMandalaChart(
+    @Arg('input') input: UpdateMandalaChartInput
+  ): Promise<typeof UpdateMandalaChartResponse> {
+    const updatedChart = await MandalaChartModel.findByIdAndUpdate(
+      input.mandalaChartId,
+      input,
+      { new: true }
+    )
+    if (!updatedChart) {
+      return { errorType: UpdateMandalaChartFailureType.CHART_NOT_FOUND }
+    }
+    return { _id: updatedChart._id }
+  }
+
+  @Mutation(() => DeleteMandalaChartResponse)
+  async deleteMandalaChart(
+    @Arg('input') input: DeleteMandalaChartInput
+  ): Promise<typeof DeleteMandalaChartResponse> {
+    const deletedChart = await MandalaChartModel.findByIdAndDelete(
+      input.mandalaChartId
+    )
+    if (!deletedChart) {
+      return { errorType: DeleteMandalaChartFailureType.CHART_NOT_FOUND }
+    }
+    return { _id: deletedChart._id }
   }
 }
