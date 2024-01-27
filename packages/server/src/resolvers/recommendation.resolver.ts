0f36b5eb024c8d247971f9651e7308cca3ec99bd
Yu Taek Lee <lcpnine@gmail.com>
2024-01-27 14:28:40 +0900

    :sparkles: Set mock data for dev

--- packages/server/src/resolvers/recommendation.resolver.ts
+++ packages/server/src/resolvers/recommendation.resolver.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata'
 import { Arg, Query, Resolver } from 'type-graphql'
+import { IS_DEV } from '../constant/common'
 import { RecommendationInNeed, getRecommendations } from '../utils/openai'
 import {
   RecommendationFailureType,
@@ -22,6 +23,29 @@ export class RecommendationResolver {
     }
 
     try {
+      if (IS_DEV)
+        return {
+          recommendations: [
+            {
+              text: 'This is a recommendation 1',
+            },
+            {
+              text: 'This is a recommendation 2',
+            },
+            {
+              text: 'This is a recommendation 3',
+            },
+            {
+              text: 'This is a recommendation 4',
+            },
+            {
+              text: 'This is a recommendation 5',
+            },
+            {
+              text: 'This is a recommendation 6',
+            },
+          ],
+        }
       const { recommendations } = await getRecommendations({
         recommendationInNeed: RecommendationInNeed.SubGoals,
         params: {
