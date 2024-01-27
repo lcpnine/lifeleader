e3c055e5f60a3ba5f315d9d37f8d1527126cf1f0
Yu Taek Lee <lcpnine@gmail.com>
2024-01-28 02:10:56 +0900

    :truck: Copy index page to chart page

---
+++ packages/web/src/pages/mandala/chart.i18n.ts
@@ -0,0 +1,25 @@
+const TRANSLATIONS = {
+  en: {
+    tabTitle: 'Life Leader | Create your own Mandala Chart',
+    title: 'Mandala Chart',
+    description: `The Mandala Chart is a powerful tool
+    for visualizing and achieving goals. It helps in organizing thoughts and
+    actions towards a specific objective.`,
+    loadSavedChart: "If you've saved your chart, click here to load.",
+  },
+  ko: {
+    tabTitle: 'Life Leader | 당신만의 만다라트 계획표를 만들어보세요',
+    title: '만다라트 계획표',
+    description: `만다라트 계획표는 목표를 성취하기 위한 강력한 도구입니다. 특정 목표를 위한
+    생각과 행동을 조직화하는 데 도움이 됩니다.`,
+    loadSavedChart: '저장한 차트가 있다면 여기를 클릭하여 불러오세요.',
+  },
+  'zh-Hant': {
+    tabTitle: 'Life Leader | 制定自己的曼陀羅思考法九宮格',
+    title: '曼陀羅思考法九宮格',
+    description: `曼陀羅思考法九宮格令夢想變得圖像化，助你早日達成理想。這種思考法可以用作組織思考和行動，一步步具體實現目標。`,
+    loadSavedChart: '如果您已經保存了圖表，請點擊此處加載。',
+  },
+}
+
+export default TRANSLATIONS
--- packages/web/src/pages/mandala/chart.page.tsx
+++ packages/web/src/pages/mandala/chart.page.tsx
@@ -1,5 +1,78 @@
+import { COMMON_TRANSLATIONS } from '@/constants/i18n'
+import { useAlert } from '@/contexts/AlertContext'
+import { useUserContext } from '@/contexts/UserContext'
+import useGoTo from '@/hooks/useGoTo'
+import useI18n from '@/hooks/useI18n'
+import useMandalaChart from '@/hooks/useMandalaChart.tsx'
+import Head from 'next/head'
+import TRANSLATIONS from './chart.i18n'
+
 const MandalaChartPage = () => {
-  return <>Hi</>
+  const { getTranslation } = useI18n()
+  const { user, isSignedIn } = useUserContext()
+  const { goTo } = useGoTo()
+  const translation = getTranslation(TRANSLATIONS)
+  const commonTranslation = getTranslation(COMMON_TRANSLATIONS)
+  const { openAlert } = useAlert()
+
+  const handleLoadSavedChartClick = () => {
+    if (!isSignedIn) {
+      openAlert({
+        text: commonTranslation('NeedToSignIn'),
+      })
+      return goTo('/auth/sign-in')
+    }
+    goTo('/mandala/my-list')
+  }
+
+  const {
+    ThemeSelector,
+    AIModeSwitch,
+    MandalaChart,
+    Recommendations,
+    ScreenShotComponent,
+    SaveChartButton,
+    DownloadImageButton,
+  } = useMandalaChart()
+
+  return (
+    <>
+      <Head>
+        <title>{translation('tabTitle')}</title>
+        <meta name="description" content={translation('description')} />
+      </Head>
+      <div className="flex flex-col items-center justify-center p-4">
+        <h1 className="text-5xl font-extrabold text-center text-blue-600 my-4 shadow-sm">
+          {translation('title')}
+        </h1>
+        <div className="flex items-center justify-center w-3/5">
+          <p className="break-words">{translation('description')}</p>
+        </div>
+        <div className="pt-4">
+          <p
+            className="inline-block px-4 py-2 text-gray-500 hover:text-gray-900 italic underline transition duration-300
+          focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-opacity-50 mb-4 hover:cursor-pointer"
+            onClick={handleLoadSavedChartClick}
+          >
+            {translation('loadSavedChart')}
+          </p>
+        </div>
+        <div className="pb-4">{ThemeSelector}</div>
+        {user.purchasedInfo.isPurchased && (
+          <div className="pt-4 flex flex-col items-center justify-center">
+            {AIModeSwitch}
+          </div>
+        )}
+        <div className="pt-4">{MandalaChart}</div>
+        <div className="pt-4 flex gap-2">
+          {SaveChartButton}
+          {DownloadImageButton}
+        </div>
+      </div>
+      {Recommendations}
+      {ScreenShotComponent}
+    </>
+  )
 }
 
 export default MandalaChartPage
