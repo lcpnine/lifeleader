9c34f135a3be08e73e07db40b9929a9160fe5564
Yu Taek Lee <lcpnine@gmail.com>
2024-01-28 16:31:21 +0900

    :sparkles: Make use go to can keep the params

--- packages/web/src/hooks/useGoTo.tsx
+++ packages/web/src/hooks/useGoTo.tsx
@@ -3,24 +3,29 @@ import { useRouter } from 'next/router'
 interface GoToOptions {
   replace?: boolean
   params?: Record<string, string>
+  keepParams?: boolean
 }
 
 const useGoTo = () => {
   const router = useRouter()
+  const currentParams = router.query
 
   const goTo = (path: string, options?: GoToOptions) => {
     const params = options?.params || {}
+    const keepParams = options?.keepParams || false
+
+    const paramsString =
+      params || (keepParams && currentParams)
+        ? Object.entries({ ...currentParams, ...params })
+            .map(([key, value]) => `${key}=${value}`)
+            .join('&')
+        : ''
+
+    const newPath = path + paramsString
+
     if (options?.replace)
-      return router.replace(
-        path + (params ? '?' + new URLSearchParams(params) : ''),
-        undefined,
-        { locale: router.locale }
-      )
-    router.push(
-      path + (params ? '?' + new URLSearchParams(params) : ''),
-      undefined,
-      { locale: router.locale }
-    )
+      return router.replace(newPath, undefined, { locale: router.locale })
+    router.push(newPath, undefined, { locale: router.locale })
   }
 
   return { goTo }
