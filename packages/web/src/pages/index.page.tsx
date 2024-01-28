345f19297509004083cae344294591fbcb67ccb4
Yu Taek Lee <lcpnine@gmail.com>
2024-01-28 18:19:54 +0900

    :sparkles: Control font size on mobile

--- packages/web/src/pages/index.page.tsx
+++ packages/web/src/pages/index.page.tsx
@@ -100,13 +100,13 @@ const Home = () => {
 
       <div className="flex justify-center items-center gap-4 mb-4">
         <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          className="md:text-xs bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           onClick={handleCreateChartClick}
         >
           Start Creating Your Mandala Chart
         </button>
         <button
-          className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+          className="md:text-xs bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
           onClick={handleSignUpClick}
         >
           Sign Up
