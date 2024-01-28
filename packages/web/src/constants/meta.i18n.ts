1807d6e1dc4c31978a18d20c06d6f07739b02dce
Yu Taek Lee <lcpnine@gmail.com>
2024-01-29 01:49:21 +0900

    :sparkles: Fix meta

--- packages/web/src/constants/meta.i18n.ts
+++ packages/web/src/constants/meta.i18n.ts
@@ -2,19 +2,16 @@ const META_TRANSLATIONS = {
   en: {
     HomeTitle: 'Life Leader',
     HomeDescription:
-      'Life Leader is a goal visualization and achievement platform. It helps you to visualize your goals, and achieve them with the help of your friends and family.',
+      'Life Leader is a platform helping you to visualize and achieve your goals.',
     DeleteAccountTitle: 'Delete Account',
-    DeleteAccountDescription:
-      'Securely delete your Life Leader account. Manage your personal data and preferences with ease.',
+    DeleteAccountDescription: 'Securely delete your Life Leader account.',
     FindPasswordTitle: 'Find Password',
     FindPasswordDescription:
       'Recover your Life Leader password easily and securely.',
     ResetPasswordTitle: 'Reset Password',
-    ResetPasswordDescription:
-      'Reset your Life Leader password securely and continue your journey towards achieving your goals.',
+    ResetPasswordDescription: 'Reset your Life Leader password securely.',
     SignInTitle: 'Sign In',
-    SignInDescription:
-      'Access your Life Leader account and continue visualizing and achieving your goals.',
+    SignInDescription: 'Access your Life Leader account.',
     SignUpTitle: 'Sign Up',
     SignUpDescription:
       'Join Life Leader and start visualizing and achieving your goals with our innovative platform.',
@@ -29,52 +26,46 @@ const META_TRANSLATIONS = {
       'View and manage all your Mandala Charts in one place.',
   },
   ko: {
-    HomeTitle: '라이프 리더',
+    HomeTitle: 'Life Leader',
     HomeDescription:
-      '라이프 리더는 목표 시각화 및 달성 플랫폼입니다. 친구와 가족의 도움을 받아 목표를 시각화하고 달성할 수 있습니다.',
+      '목표를 시각화하고 달성하는 데 도움이 되는 도구를 제공하는 Life Leader입니다.',
     DeleteAccountTitle: '계정 삭제',
-    DeleteAccountDescription:
-      '라이프 리더 계정을 안전하게 삭제하세요. 개인 데이터 및 선호도를 쉽게 관리할 수 있습니다.',
+    DeleteAccountDescription: 'Life Leader 계정을 안전하게 삭제하세요.',
     FindPasswordTitle: '비밀번호 찾기',
     FindPasswordDescription:
-      '라이프 리더의 비밀번호를 쉽고 안전하게 복구하세요.',
+      'Life Leader의 비밀번호를 쉽고 안전하게 복구하세요.',
     ResetPasswordTitle: '비밀번호 재설정',
-    ResetPasswordDescription:
-      '라이프 리더 비밀번호를 안전하게 재설정하고 목표 달성 여정을 계속하세요.',
+    ResetPasswordDescription: 'Life Leader 비밀번호를 안전하게 재설정하세요.',
     SignInTitle: '로그인',
-    SignInDescription:
-      '라이프 리더 계정에 접속하여 목표 시각화 및 달성을 계속하세요.',
+    SignInDescription: 'Life Leader 계정에 접속하세요.',
     SignUpTitle: '회원 가입',
     SignUpDescription:
-      '라이프 리더에 가입하고 혁신적인 플랫폼으로 목표를 시각화하고 달성하세요.',
+      'Life Leader에 가입하고 혁신적인 플랫폼으로 목표를 시각화하고 달성하세요.',
     VerifyEmailTitle: '이메일 인증',
     VerifyEmailDescription:
-      '이메일을 인증하여 라이프 리더와의 여정을 계속하세요.',
+      '이메일을 인증하여 Life Leader와의 여정을 계속하세요.',
     MandalaChartTitle: '만다라 차트',
     MandalaChartDescription:
-      '목표 설정과 시각화를 위한 강력한 도구인 만다라 차트를 만들고 봅니다.',
+      '목표 설정 및 시각화를 위한 강력한 도구인 만다라 차트를 만들어 보세요.',
     MyMandalaChartsTitle: '내 만다라 차트',
     MyMandalaChartsDescription: '한 곳에서 모든 만다라 차트를 보고 관리하세요.',
   },
   'zh-Hant': {
-    HomeTitle: '生活領袖',
-    HomeDescription:
-      '生活領袖是一個目標視覺化和實現平台。它幫助您與朋友和家人的幫助下，視覺化您的目標並實現它們。',
+    HomeTitle: 'Life Leader',
+    HomeDescription: 'Life Leader是一個平台，幫助您視覺化和實現您的目標。',
     DeleteAccountTitle: '刪除帳戶',
-    DeleteAccountDescription:
-      '安全地刪除您的生活領袖帳戶。輕鬆管理您的個人資料和偏好設定。',
+    DeleteAccountDescription: '安全地刪除您的Life Leader帳戶。',
     FindPasswordTitle: '找回密碼',
-    FindPasswordDescription: '輕鬆安全地恢復您的生活領袖密碼。',
+    FindPasswordDescription: '輕鬆安全地恢復您的Life Leader密碼。',
     ResetPasswordTitle: '重設密碼',
-    ResetPasswordDescription:
-      '安全地重設您的生活領袖密碼，繼續您實現目標的旅程。',
+    ResetPasswordDescription: '安全地重設您的Life Leader密碼。',
     SignInTitle: '登入',
-    SignInDescription: '登入您的生活領袖帳戶，繼續視覺化和實現您的目標。',
+    SignInDescription: '訪問您的Life Leader帳戶。',
     SignUpTitle: '註冊',
     SignUpDescription:
-      '加入生活領袖，開始使用我們的創新平台視覺化和實現您的目標。',
+      '加入Life Leader，開始使用我們的創新平台視覺化和實現您的目標。',
     VerifyEmailTitle: '驗證電子郵件',
-    VerifyEmailDescription: '驗證您的電子郵件，繼續與生活領袖的旅程。',
+    VerifyEmailDescription: '驗證您的電子郵件，繼續與Life Leader的旅程。',
     MandalaChartTitle: '曼陀羅圖',
     MandalaChartDescription:
       '創建和查看您的曼陀羅圖，這是設定和視覺化您的目標的強大工具。',
