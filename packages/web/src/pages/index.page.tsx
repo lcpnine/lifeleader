aa890a0f4807ddab407219a483b78aa4ccdd374e
Yu Taek Lee <lcpnine@gmail.com>
2024-01-28 23:11:35 +0900

    :sparkles: Add i18n on index page

--- packages/web/src/pages/index.page.tsx
+++ packages/web/src/pages/index.page.tsx
@@ -9,11 +9,13 @@ import KoreanMandalaChartExample from 'public/images/example/ko.png'
 import KoreanMandalaEmptyChartExample from 'public/images/example/ko_empty.png'
 import ChineseTraditioanlMandalaChartExample from 'public/images/example/zh.png'
 import ChineseTraditioanlMandalaEmptyChartExample from 'public/images/example/zh_empty.png'
+import TRANSLATIONS from './index.i18n'
 
 const Home = () => {
-  const { currentLanguage } = useI18n()
+  const { currentLanguage, getTranslation } = useI18n()
   const { isSignedIn } = useUserContext()
   const { goTo } = useGoTo()
+  const translation = getTranslation(TRANSLATIONS)
 
   const getExampleChart = () => {
     if (currentLanguage === SUPPORTING_LANGUAGES.ko)
@@ -46,37 +48,21 @@ const Home = () => {
   return (
     <div className="p-4 max-w-4xl mx-auto">
       <h1 className="text-3xl font-bold mb-6 text-center">
-        Welcome to Life Leader
+        {translation('WelcomeToLifeLeader')}
       </h1>
-      <p className="mb-8 text-lg">
-        At Life Leader, we're dedicated to providing tools that not only help
-        you visualize your goals but also support you in achieving your dreams.
-        With our latest offerings, including the insightful Mandala Chart and an
-        AI recommendation system powered by GPT-4, we aim to guide you on your
-        journey to success.
-      </p>
+      <p className="mb-8 text-lg">{translation('WelcomingParagraph')}</p>
       <section className="mb-8">
         <h2 className="text-2xl font-semibold mb-4 text-center">
-          What is a Mandala Chart?
+          {translation('WhatIsMandalaChart')}
         </h2>
-        <p className="mb-4">
-          The Mandala Chart, a concept developed in Japan, is a dynamic
-          framework for organizing thoughts and goals. It's a versatile tool for
-          breaking down complex objectives into smaller, actionable components,
-          aiding in comprehensive planning and execution.
-        </p>
+        <p className="mb-4">{translation('MandalaChartExplanation')}</p>
       </section>
 
       <section className="mb-8">
         <h2 className="text-2xl font-semibold mb-4">
-          Structure of a Mandala Chart
+          {translation('StructureOfMandalaChart')}
         </h2>
-        <p className="mb-4">
-          The Mandala Chart is visually represented as a large square divided
-          into nine smaller squares. The central square encapsulates the primary
-          goal or theme, while the eight surrounding squares represent related
-          sub-goals or aspects, offering a holistic perspective.
-        </p>
+        <p className="mb-4">{translation('StructureExplanation')}</p>
         <div className="flex justify-center">
           <Image
             src={getExampleEmptyChart().src}
@@ -89,14 +75,9 @@ const Home = () => {
 
       <section className="mb-8">
         <h2 className="text-2xl font-semibold mb-4">
-          Creating Your Mandala Chart
+          {translation('CreatingYourMandalaChart')}
         </h2>
-        <p className="mb-4">
-          Begin by placing your central goal in the middle square. Surround it
-          with eight related elements, each representing a sub-goal or aspect.
-          Elaborate each section with specific actions or thoughts, creating a
-          detailed roadmap for your objective.
-        </p>
+        <p className="mb-4">{translation('CreatingChartExplanation')}</p>
         <div className="flex justify-center">
           <Image
             src={getExampleChart().src}
@@ -109,37 +90,22 @@ const Home = () => {
 
       <section className="mb-8">
         <h2 className="text-2xl font-semibold mb-4 text-center">
-          Benefits of Using a Mandala Chart
+          {translation('BenefitsOfUsingMandalaChart')}
         </h2>
-        <p className="mb-4">
-          Utilizing a Mandala Chart ensures a comprehensive view of your goals,
-          promoting structured thinking and clarity. It's an invaluable tool for
-          both personal and professional growth, adaptable to diverse scenarios
-          and objectives.
-        </p>
+        <p className="mb-4">{translation('BenefitsExplanation')}</p>
       </section>
 
       <section className="mb-8">
         <h2 className="text-2xl font-semibold mb-4 text-center">
-          Adapting the Chart to Your Needs
+          {translation('AdaptingChartToYourNeeds')}
         </h2>
         <p className="mb-4">
-          The Mandala Chart's versatility lies in its adaptability. Whether for
-          educational purposes, career planning, or personal development, the
-          chart can be customized to align with your unique aspirations and
-          challenges.
+          {translation('AdaptingChartToYourNeedsExplanation')}
         </p>
       </section>
 
       <section className="mb-8">
-        <p className="mb-4 text-lg">
-          By signing up with Life Leader, you gain the ability to save, modify,
-          and revisit your Mandala Charts at any time. Plus, as part of our
-          opening event, new sign-ups can now fully utilize the Chat GPT
-          recommendation feature without any charge until February. Don’t miss
-          this opportunity to enhance your journey to success with our advanced
-          tools.
-        </p>
+        <p className="mb-4 text-lg">{translation('SignUpInvitation')}</p>
       </section>
 
       <div className="flex justify-center items-center gap-4 mb-4">
@@ -147,14 +113,14 @@ const Home = () => {
           className="sm:text-xs md:text-base bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           onClick={handleCreateChartClick}
         >
-          Start Creating Your Mandala Chart
+          {translation('StartCreatingYourMandalaChart')}
         </button>
         {!isSignedIn && (
           <button
             className="sm:text-xs md:text-base bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
             onClick={handleSignUpClick}
           >
-            Sign Up
+            {translation('SignUp')}
           </button>
         )}
       </div>
@@ -165,7 +131,7 @@ const Home = () => {
             className="text-blue-600 hover:text-blue-800 visited:text-purple-800"
             onClick={handleSignInClick}
           >
-            Registered already? Sign in to your account.
+            {translation('AlreadyRegisteredSignIn')}
           </a>
         </div>
       )}
