ef573aeec977ccb78f0db030aff0f96f6427eac2
Yu Taek Lee <lcpnine@gmail.com>
2024-01-31 23:21:19 +0900

    :recycle: Change description

--- packages/web/src/components/MandalaThemeSelector/MandalaThemeSelector.i18n.ts
+++ packages/web/src/components/MandalaThemeSelector/MandalaThemeSelector.i18n.ts
@@ -1,15 +1,15 @@
 const TRANSLATIONS = {
   en: {
     Description: 'Select a theme to personalize your Mandala Chart',
-    SmallDescription: "Selected Theme won't be saved",
+    SmallDescription: "Selected theme won't be saved together with your chart",
   },
   ko: {
     Description: '원하는 테마를 선택해보세요',
-    SmallDescription: '선택한 테마는 저장되지 않습니다',
+    SmallDescription: '선택된 테마 정보는 함께 저장되지 않습니다',
   },
   'zh-Hant': {
     Description: '為你的曼陀羅思考法九宮格選擇個人風格',
-    SmallDescription: '選擇的風格不會被保存',
+    SmallDescription: '選擇的風格不會與您的圖表一起保存',
   },
 }
 
