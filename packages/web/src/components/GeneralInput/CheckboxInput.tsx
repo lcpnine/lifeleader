45b803906b96ae25af091eecfcf882098ee1048d
Yu Taek Lee <lcpnine@gmail.com>
2024-04-08 01:06:07 +0900

    :lipstick: Change checkbox style

--- packages/web/src/components/GeneralInput/CheckboxInput.tsx
+++ packages/web/src/components/GeneralInput/CheckboxInput.tsx
@@ -2,34 +2,28 @@ import { ChangeEventHandler } from 'react'
 
 interface Props {
   label?: string
-  placeholder: string
   value: string
   onChange: ChangeEventHandler<HTMLInputElement>
   guide?: string
 }
 
-const CheckboxInput = ({
-  label,
-  placeholder,
-  value,
-  onChange,
-  guide,
-}: Props) => {
+const CheckboxInput = ({ label, value, onChange, guide }: Props) => {
   return (
     <div className="mb-4">
-      {label && (
-        <label className="block text-gray-700 text-sm font-bold mb-1">
-          {label}
-        </label>
-      )}
-      {guide && <div className="text-gray-600 text-sm mb-1">{guide}</div>}
-      <input
-        type="checkbox"
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-        className="border p-2 w-full"
-      />
+      <div className="flex items-center">
+        <input
+          type="checkbox"
+          value={value}
+          onChange={onChange}
+          className="border p-2"
+        />
+        {label && (
+          <label className="ml-2 text-gray-700 text-sm font-bold">
+            {label}
+          </label>
+        )}
+      </div>
+      {guide && <div className="text-gray-600 text-sm mt-1">{guide}</div>}
     </div>
   )
 }
