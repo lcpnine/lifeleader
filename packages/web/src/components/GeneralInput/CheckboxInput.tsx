f4378184f101d59a18f4f43e78c76acfc36b8f9f
Yu Taek Lee <lcpnine@gmail.com>
2024-04-08 01:22:07 +0900

    :sparkles: Add link prop

--- packages/web/src/components/GeneralInput/CheckboxInput.tsx
+++ packages/web/src/components/GeneralInput/CheckboxInput.tsx
@@ -4,10 +4,11 @@ interface Props {
   label?: string
   value: string
   onChange: ChangeEventHandler<HTMLInputElement>
+  link?: string
   guide?: string
 }
 
-const CheckboxInput = ({ label, value, onChange, guide }: Props) => {
+const CheckboxInput = ({ label, value, onChange, link, guide }: Props) => {
   return (
     <div className="mb-4">
       <div className="flex items-center">
@@ -18,7 +19,12 @@ const CheckboxInput = ({ label, value, onChange, guide }: Props) => {
           className="border p-2"
         />
         {label && (
-          <label className="ml-2 text-gray-700 text-sm font-bold">
+          <label
+            className={`ml-2 text-gray-700 text-sm font-bold ${
+              link ? 'cursor-pointer hover:text-blue-700 hover:underline' : ''
+            }`}
+            onClick={() => link && window.open(link, '_blank')}
+          >
             {label}
           </label>
         )}
